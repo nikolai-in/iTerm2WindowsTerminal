08f5d70a23512da04e6036d2eb8f42a3940ee633
nikolai-in <nikolai-in@tuta.com>
2025-06-21 18:49:32 +0300

    fix: 🐛 Claude made it work with monokai pro schemes
    I have no time for this, hate powershell and have no idea what was the problem

--- ConvertFrom-iTerm2.ps1
+++ ConvertFrom-iTerm2.ps1
@@ -1,15 +1,15 @@
 param(
-    [Parameter(Mandatory=$true,Position=0,
-    ParameterSetName="Internet",
-    ValueFromPipelineByPropertyName=$true,
-    HelpMessage="The URL with iTerm colours.")]
+    [Parameter(Mandatory = $true, Position = 0,
+        ParameterSetName = "Internet",
+        ValueFromPipelineByPropertyName = $true,
+        HelpMessage = "The URL with iTerm colours.")]
     [ValidateScript({ $_ -match '^http.*itermcolors$' })]
     [string]$colorFileURL,
 
-    [Parameter(Mandatory=$true,Position=0,
-    ParameterSetName="Local",
-    ValueFromPipelineByPropertyName=$true,
-    HelpMessage="A file iTerm colours.")]
+    [Parameter(Mandatory = $true, Position = 0,
+        ParameterSetName = "Local",
+        ValueFromPipelineByPropertyName = $true,
+        HelpMessage = "A file iTerm colours.")]
     [ValidateScript({ ([xml](Get-Content $_) -is [xml]) })]
     [string]$colorFile
 )
@@ -26,53 +26,63 @@ function ConvertTerm2Terminals {
     )
 
     $colorMappings = @{
-        "Ansi 0 Color" = "black"
-        "Ansi 1 Color" = "red"
-        "Ansi 2 Color" = "green"
-        "Ansi 3 Color" = "yellow"
-        "Ansi 4 Color" = "blue"
-        "Ansi 5 Color" = "purple" # I can't find magenta in the VSCode colors, so I go with purple
-        "Ansi 6 Color" = "cyan"
-        "Ansi 7 Color" = "white"
-        "Ansi 8 Color" = "brightBlack"
-        "Ansi 9 Color" = "brightRed"
-        "Ansi 10 Color" = "brightGreen"
-        "Ansi 11 Color" = "brightYellow"
-        "Ansi 12 Color" = "brightBlue"
-        "Ansi 13 Color" = "brightPurple"
-        "Ansi 14 Color" = "brightCyan"
-        "Ansi 15 Color" = "brightWhite"
-        "Cursor Color" = "cursorColor"
-        "Cursor Text Color" = "!cursorTextColor" # I made this up. Ignore it. 
-        "Bold Color" = "!boldColor" # I made this up. Ignore it. 
+        "Ansi 0 Color"        = "black"
+        "Ansi 1 Color"        = "red"
+        "Ansi 2 Color"        = "green"
+        "Ansi 3 Color"        = "yellow"
+        "Ansi 4 Color"        = "blue"
+        "Ansi 5 Color"        = "purple" # I can't find magenta in the VSCode colors, so I go with purple
+        "Ansi 6 Color"        = "cyan"
+        "Ansi 7 Color"        = "white"
+        "Ansi 8 Color"        = "brightBlack"
+        "Ansi 9 Color"        = "brightRed"
+        "Ansi 10 Color"       = "brightGreen"
+        "Ansi 11 Color"       = "brightYellow"
+        "Ansi 12 Color"       = "brightBlue"
+        "Ansi 13 Color"       = "brightPurple"
+        "Ansi 14 Color"       = "brightCyan"
+        "Ansi 15 Color"       = "brightWhite"
+        "Cursor Color"        = "cursorColor"
+        "Cursor Text Color"   = "!cursorTextColor" # I made this up. Ignore it. 
+        "Bold Color"          = "!boldColor" # I made this up. Ignore it. 
         "Selected Text Color" = "!selectionForeground" # I made this up. Ignore it. 
-        "Selection Color" = "selectionBackground"
-        "Background Color" = "background"
-        "Foreground Color" = "foreground"
+        "Selection Color"     = "selectionBackground"
+        "Background Color"    = "background"
+        "Foreground Color"    = "foreground"
     }
 
     $colorMappings.$iTermColor
 }
 
-# function that takes as input an array of floating point numbers
-# it expects the first element to be the Blue, second to be Green, and last to be Red
+# function that takes as input a dict containing color components
+# and outputs a Windows Terminal compatible hex color
 function ConvertReal2Hex {
     [CmdletBinding()]
     param(
         [Parameter()]
-        [float[]]$Real
+        [System.Xml.XmlElement]$ColorDict
     )
 
-    # I take each decimal, multiply by 255, and cast to integer. This is because the -f operator is expecting an integer. 
-    # Learnt this through trial and error. 
-    [int]$Blue = $Real[0] * 255
-    [int]$Green = $Real[1] * 255
-    [int]$Red = $Real[2] * 255
+    try {
+        # Extract individual color components from the XML dict
+        # iTerm stores colors as floating point values between 0 and 1
+        $blueComponent = [float]($ColorDict.SelectSingleNode("real[preceding-sibling::key[1][text()='Blue Component']]").InnerText)
+        $greenComponent = [float]($ColorDict.SelectSingleNode("real[preceding-sibling::key[1][text()='Green Component']]").InnerText)
+        $redComponent = [float]($ColorDict.SelectSingleNode("real[preceding-sibling::key[1][text()='Red Component']]").InnerText)
 
-    "#{0:X2}{1:X2}{2:X2}" -f $Red,$Green,$Blue
-    # The 0, 1, 2 stand for the place holder. The right side of the -f operator is an array, so 0, 1, 2 refer to the objects of this array. 
-    # Basically, one can mix up the array elements on the output (left side). The "X" tells the format operator to convert to a hex. 
-    # The 2 after the X tells it to do 2 places. I realized this when some of the output didn't have 2 places. 
+        # Convert from 0-1 range to 0-255 range and ensure we have integers
+        # Clamp values to valid range in case of any parsing issues
+        [int]$red = [Math]::Max(0, [Math]::Min(255, [Math]::Round($redComponent * 255)))
+        [int]$green = [Math]::Max(0, [Math]::Min(255, [Math]::Round($greenComponent * 255)))
+        [int]$blue = [Math]::Max(0, [Math]::Min(255, [Math]::Round($blueComponent * 255)))
+
+        # Format as hex color (#RRGGBB)
+        "#{0:X2}{1:X2}{2:X2}" -f $red, $green, $blue
+    }
+    catch {
+        Write-Warning "Failed to parse color components: $($_.Exception.Message)"
+        return "#000000"  # Default to black on error
+    }
 }
 
 $finalOutput = @{}
@@ -80,12 +90,14 @@ $finalOutput = @{}
 if ($colorFileURL) {
     try {
         [xml]$xmlObj = $(Invoke-WebRequest $colorFileURL).Content
-    } catch {
+    }
+    catch {
         throw $_.Exception.Message
     }
     $finalOutput["name"] = $(Split-Path $colorFileURL -LeafBase)
 
-} else {
+}
+else {
     [xml]$xmlObj = [xml](Get-Content $colorFile)
     $finalOutput["name"] = $(Split-Path $colorFile -LeafBase)
 }
@@ -93,12 +105,15 @@ if ($colorFileURL) {
 $keysArray = @($xmlObj.plist.dict.key)
 $valuesArray = @($xmlObj.plist.dict.dict)
 
-$hexColorsArray = foreach ($value in $valuesArray) { ConvertReal2Hex $value.real }
+$hexColorsArray = foreach ($value in $valuesArray) { ConvertReal2Hex $value }
 $winColorNamesArray = foreach ($key in $keysArray) { ConvertTerm2Terminals $key }
 
 
 for ($i = 0; $i -lt $winColorNamesArray.Length; $i++) {
-    if ($winColorNamesArray[$i] -notmatch "^!") { $finalOutput[$winColorNamesArray["$i"]] = $hexColorsArray[$i] }
+    $colorName = $winColorNamesArray[$i]
+    if ($colorName -and $colorName -notmatch "^!" -and $hexColorsArray[$i]) { 
+        $finalOutput[$colorName] = $hexColorsArray[$i] 
+    }
 }
 
 $finalOutput | ConvertTo-Json
